2530da6bf2a475fb60332d87db8618586c13013a
Heitor Teixeira <heitor.teixeira@inallmedia.com>
2023-09-04 17:51:17 -0300

    Creating system initial structure

---
+++ src/main/java/com/iam/serviceacquisition/ServiceAcquisitionApplication.java
@@ -0,0 +1,13 @@
+package com.iam.serviceacquisition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAcquisitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAcquisitionApplication.class, args);
+	}
+
+}
