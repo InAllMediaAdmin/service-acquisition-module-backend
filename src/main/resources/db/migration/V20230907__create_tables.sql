1c390281b59364f5783c4a2cd0c083215f86f8f8
Luciano Adassus <luciano.adassus@inallmedia.com>
2023-09-07 16:51:51 -0300

    changes on script initial flyway
    Signed-off-by: Luciano Adassus <luciano.adassus@inallmedia.com>

--- src/main/resources/db/migration/V20230907__create_tables.sql
+++ src/main/resources/db/migration/V20230907__create_tables.sql
@@ -18,17 +18,7 @@ create table if not exists country
 create index if not exists flyway_schema_history_s_idx
     on flyway_schema_history (success);
 
-create table if not exists hibernate_sequence
-(
-    next_not_cached_value bigint(21)          not null,
-    minimum_value         bigint(21)          not null,
-    maximum_value         bigint(21)          not null,
-    start_value           bigint(21)          not null comment 'start value when sequences is created or value if RESTART is used',
-    increment             bigint(21)          not null comment 'increment value',
-    cache_size            bigint(21) unsigned not null,
-    cycle_option          tinyint(1) unsigned not null comment '0 if no cycles are allowed, 1 if the sequence should begin a new cycle when maximum_value is passed',
-    cycle_count           bigint(21)          not null comment 'How many cycles have been done'
-    );
+create sequence hibernate_sequence MINVALUE 1000 START WITH 1000;
      
 
 create table if not exists industry
